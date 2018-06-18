1fa127fdd4105ed37dff1f181b2083cd069ce7d2
Tom Carrio <Thomas.Carrio@Ally.com>
2018-06-18 15:24:30 -0400

    Updated to use MenuService

--- src/main/java/me/carrio/SpringJokes/SpringJokesApplication.java
+++ src/main/java/me/carrio/SpringJokes/SpringJokesApplication.java
@@ -1,6 +1,7 @@
 package me.carrio.SpringJokes;
 
-import java.util.Scanner;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,12 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import me.carrio.SpringJokes.entity.Joke;
 import me.carrio.SpringJokes.repository.JokesRepository;
+import me.carrio.SpringJokes.service.MenuService;
 
 @SpringBootApplication
 public class SpringJokesApplication implements CommandLineRunner {
 
 	@Autowired
 	private JokesRepository jokeRepository;
+	@Autowired
+	private MenuService menuService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJokesApplication.class, args);
@@ -25,27 +29,64 @@ public class SpringJokesApplication implements CommandLineRunner {
     public void run(String... strings) throws Exception {
 		System.out.printf("The database contains %s jokes.\n", jokeRepository.count());
 
-	    // I'm not using the multi-argument constructor so that I can use scanner
-		Scanner scanner = new Scanner(System.in);
-	    // to read values directly into the joke properties.
-	    Joke joke = new Joke();
+		while(true){
+	        int primaryAction = menuService.promptForMainMenuSelection();
 
-	    // read the joke data
-	    System.out.println("Tell us a joke: ");
-	    joke.setJoke(scanner.nextLine());
+	        if(primaryAction == MenuService.LIST_JOKES){
+	            List<Joke> jokes = jokeRepository.findAll();
+	            menuService.displayJokes(jokes);
 
-	    System.out.println("What's the punchline?: ");
-	    joke.setPunchline(scanner.nextLine());
+	        } else if(primaryAction == MenuService.CREATE_JOKE){
+	            // collect the joke data
+	            Joke joke = menuService.promptForJokeData();
 
-	    System.out.println("On a scale of 1 to 10, how funny is this joke?");
-	    // for the sake of simplicity I'm not validating this input
-	    joke.setRating(scanner.nextInt());
+	            // save the joke
+	            jokeRepository.save(joke);
 
-	    // save the joke
-	    jokeRepository.save(joke);
+	        } else if(primaryAction == MenuService.VIEW_JOKE){
+	            // which joke?
+	            int id = menuService.promptForJokeID();
 
-	    // how many jokes do we have now?
-	    System.out.printf("The database contains %s jokes.\n", jokeRepository.count());
-	    scanner.close();
+	            // read the joke
+	            Optional<Joke> jokeOpt = jokeRepository.findById(id);
+
+	            if(jokeOpt.isPresent()){
+	                // display the joke
+	                menuService.displayJoke(jokeOpt.get());
+	            } else {
+	                menuService.displayNoSuchJoke();
+	            }
+
+	        } else if(primaryAction == MenuService.EDIT_JOKE){
+	            // which joke?
+	            int index = menuService.promptForJokeID();
+
+	            // read the joke
+	            Optional<Joke> jokeOpt = jokeRepository.findById(index);
+
+	            if(jokeOpt.isPresent()){
+	                // update the joke data
+	                Joke joke = menuService.promptForJokeData(jokeOpt.get());
+
+	                // update the joke
+	                jokeRepository.save(joke);
+	            } else {
+	                menuService.displayNoSuchJoke();
+	            }
+
+	        } else if(primaryAction == MenuService.DELETE_JOKE){
+	            // which joke?
+	            int index = menuService.promptForJokeID();
+
+	            // delete the joke
+	            jokeRepository.deleteById(index);
+
+	        } else if(primaryAction == MenuService.QUIT){
+
+	            // break out of the loop
+	            break;
+
+	        }
+	    }
     }
 }
